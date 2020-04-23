1e921ec042aabbf0ad538b87afbe085047283cad
Brendan Jackson <brendanjj50@gmail.com>
2020-04-23 20:14:02 +0200

    Updates Index

---
+++ JS/gallery.js
@@ -0,0 +1,13 @@
+const nextbutton = document.getElementById("next")
+
+const prevbutton = document.getElementById("prev")
+
+nextbutton.addEventListener("click",function(){
+    let el = document.getElementById("Slideshow")
+
+})
+
+prevbutton.addEventListener("click",function(){
+    let el = document.getElementById("Slideshow")
+
+})

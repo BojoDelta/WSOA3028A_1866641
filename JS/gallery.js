ff14081e398ff0ca1b2cd5123ecb77e494686509
Brendan Jackson <brendanjj50@gmail.com>
2020-04-23 22:16:07 +0200

    Added Slideshow Functionality

--- JS/gallery.js
+++ JS/gallery.js
@@ -4,11 +4,11 @@ const prevbutton = document.getElementById("prev")
 
 nextbutton.addEventListener("click",function(){
     let el = document.getElementById("Slideshow");
-    
+    el.src="../Images/CharImage1.jpg";
 
 })
 
 prevbutton.addEventListener("click",function(){
     let el = document.getElementById("Slideshow")
-
+    el.src="../Images/CharImage2.jpg";
 })

92f6da445c12c6ef423268fa73c501f65661e59e
Brendan Jackson <brendanjj50@gmail.com>
2020-04-26 14:36:27 +0200

    Update gallery.js
    Slideshow functionality

--- JS/gallery.js
+++ JS/gallery.js
@@ -2,13 +2,29 @@ const nextbutton = document.getElementById("next")
 
 const prevbutton = document.getElementById("prev")
 
+let i = 1;
+
 nextbutton.addEventListener("click",function(){
+    i = i + 1;
+    if (i===10) {
+        i = 1;  
+    }
+    console.log(i);
+    let x = "../Images/CharImage";
+    //x.concat(i,".jpg");
     let el = document.getElementById("Slideshow");
-    el.src="../Images/CharImage1.jpg";
+    el.src=x.concat(i,".jpg");
 
 })
 
 prevbutton.addEventListener("click",function(){
-    let el = document.getElementById("Slideshow")
-    el.src="../Images/CharImage2.jpg";
+    i = i - 1;
+    if (i===0) {
+        i = 9;  
+    }
+    console.log(i);
+    let x = "../Images/CharImage";
+    //x.concat(i,".jpg");
+    let el = document.getElementById("Slideshow");
+    el.src=x.concat(i,".jpg");
 })

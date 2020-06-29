a954f70bcc39516604b7b13831e885a4ec811a2a
Brendan Jackson <brendanjj50@gmail.com>
2020-06-29 12:19:22 +0200

    Updated Videos and Back Buttons

--- JS/Nav.js
+++ JS/Nav.js
@@ -24,14 +24,39 @@ const initMenu = () => {
     nav.appendChild(ul);
 }
 
+const initBlogBack = () =>{
+    //console.log("test");
+    const foot = document.getElementsByClassName("BlogDate");
+    //console.log(foot[0]);
+    const bb = document.createElement("button");
+    bb.innerText = "Back";
+    bb.className = "BackButton";
+    bb.id = "backBlog";
+    bb.addEventListener("click",function() {
+        window.history.back();
+    })
+    foot[0].appendChild(bb);
+}
+
 const Backbutton = document.getElementById("back")
 Backbutton.addEventListener("click", function () {
     window.history.back();
 })
 
+/*const BackBlogbutton = document.getElementById("backBlog")
+if (typeof(BackBlogbutton) != undefined && BackBlogbutton !== null)
+{
+    BackBlogbutton.addEventListener("click", function () {
+    window.history.back();
+})
+}*/
+
+
+
 //Attempted to use DOMContentLoaded however it did not work, I realise that 
 //using load will only perform this operation once everything is loaded, but it was only way I could get it to work
 window.addEventListener("load", () => {
     initMenu();
+    initBlogBack();
 })
 
